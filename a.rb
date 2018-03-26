ba49562d139116403ffc9cedcc6ed3b9374abb17
s151003 <s151003@kashiwanoha.ed.jp>
2018-03-26 10:12:20 +0000

    100以下に対応した 修正

--- a.rb
+++ a.rb
@@ -1,13 +1,15 @@
 num = rand(1..9999)
 
-if num < 1000 and num > 100 then
+
+if num < 1000 and num >= 100 then
 	num = num.to_s
 	num = '0' + num
-elsif num < 100 then
+elsif num < 100 and num >= 10 then
 	num = num.to_s
 	num = '00' + num
-else
+elsif num < 10 then
 	num = num.to_s
+	num = '000' + num
 end
 
 puts num
