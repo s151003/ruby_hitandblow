3f5712d5764b95e522e8923f548643f5f634d5d0
s151003 <s151003@kashiwanoha.ed.jp>
2018-03-26 09:12:09 +0000

    1000以下を型変換して0を追加する

--- a.rb
+++ a.rb
@@ -1,2 +1,13 @@
 num = rand(1..9999)
-puts(num)
+
+if num < 1000 and num > 100 then
+	num = num.to_s
+	num = '0' + num
+elsif num < 100 then
+	num = num.to_s
+	num = '00' + num
+else
+	num = num.to_s
+end
+
+puts num
